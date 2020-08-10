d497ad3e05bbceabb524785807dad53da1d19efa
Cosme Barbosa <cosmeg5@hotmail.com>
2020-08-10 14:12:21 -0300

    Criando fim do jogo com colisão da cobrinha nela mesmo.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let canvas = document.getElementById('snake');
 let context = canvas.getContext("2d");
-let box =32;
+let box = 32;
 let snake = [];
 snake[0] = {
     x: 8 *box,
@@ -31,6 +31,7 @@ function drawFood() {
 
 document.addEventListener('keydown', update);
 
+//Movimento das teclas direcionais
 function update(event) {
     if(event.keyCode == 37 && direction != "right") direction ="left";
     if(event.keyCode == 38 && direction != "down") direction ="up";
@@ -38,11 +39,20 @@ function update(event) {
     if(event.keyCode == 40 && direction != "up") direction ="down";
 }
 
+//Movimento da cobrinha
 function iniciarJogo() {
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    // Colisão da cobrinha nela mesmo.
+    for(i = 1; i < snake.length; i++) {
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
 
     criarBG();
     criarCobrinha();
@@ -56,7 +66,14 @@ function iniciarJogo() {
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y) {
+        snake.pop();
+    }
+    else { 
+        food.x = Math.floor(Math.random() * 15 + 1 ) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
+
 
     let newHead = {
         x: snakeX,
